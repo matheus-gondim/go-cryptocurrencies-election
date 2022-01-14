1b47f93f60e214b51924cab3803a1a200cedec25
matheus-gondim <matheus-pg1@hotmail.com>
2022-01-14 11:32:42 -0300

    created the functions to mapper the return and response objects

--- src/entity/cryptocurrency.go
+++ src/entity/cryptocurrency.go
@@ -2,6 +2,8 @@ package entity
 
 import (
 	"time"
+
+	"github.com/matheus-gondim/go-cryptocurrencies-election/src/pb"
 )
 
 type Cryptocurrency struct {
@@ -12,4 +14,15 @@ type Cryptocurrency struct {
 	Dislike   int64     `json:"dislike"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (c *Cryptocurrency) FromCreate(dto *pb.CreateCryptocurrency) *Cryptocurrency {
+	c.Name = dto.GetName()
+	c.Symbol = dto.GetSymbol()
+
+	return c
+}
+
+func (c *Cryptocurrency) ToOutput() *pb.Cryptocurrency {
+	return &pb.Cryptocurrency{Id: c.ID, Name: c.Name, Symbol: c.Symbol, Like: c.Like, Dislike: c.Dislike}
 }
