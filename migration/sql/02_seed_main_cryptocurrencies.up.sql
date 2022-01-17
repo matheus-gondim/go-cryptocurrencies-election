6d32a0e73b46782277f46cfbcea51b3eaf817e27
matheus-gondim <matheus-pg1@hotmail.com>
2022-01-16 23:42:52 -0300

    added the seed of the main cryptocurrencies

---
+++ migration/sql/02_seed_main_cryptocurrencies.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "cryptocurrencies"("id", "name", "symbol") VALUES (1, 'Bitcoin', 'BTC');
+INSERT INTO "cryptocurrencies"("id", "name", "symbol") VALUES (2, 'Ethereum', 'ETH');
+INSERT INTO "cryptocurrencies"("id", "name", "symbol") VALUES (3, 'Litecoin', 'LTC');
+INSERT INTO "cryptocurrencies"("id", "name", "symbol") VALUES (4, 'Ripple', 'XRP');
+INSERT INTO "cryptocurrencies"("id", "name", "symbol") VALUES (5, 'Binance coin', 'BNB');
